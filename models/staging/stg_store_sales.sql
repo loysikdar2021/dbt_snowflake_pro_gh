5b0dec566c3fb11e2e4c2baaef780b2121e5c702
Loy Sikdar <loysikdar@gmail.com>
2025-08-31 12:45:20 +0000

    Azx

--- models/staging/stg_store_sales.sql
+++ models/staging/stg_store_sales.sql
@@ -1,15 +1,16 @@
 {{ config(materialized='view') }}
+
 select
-  s_sold_date_sk       as sold_date_sk,
-  s_item_sk            as item_sk,
-  s_customer_sk        as customer_sk,
-  s_store_sk           as store_sk,
-  s_ticket_number      as ticket_number,
-  s_quantity           as quantity,
-  s_wholesale_cost     as wholesale_cost,
-  s_list_price         as list_price,
-  s_sales_price        as sales_price,
-  s_net_paid           as net_paid,
-  s_net_paid_inc_tax   as net_paid_inc_tax,
-  s_net_profit         as net_profit
+  ss_sold_date_sk       as sold_date_sk,
+  ss_item_sk            as item_sk,
+  ss_customer_sk        as customer_sk,
+  ss_store_sk           as store_sk,
+  ss_ticket_number      as ticket_number,
+  ss_quantity           as quantity,
+  ss_wholesale_cost     as wholesale_cost,
+  ss_list_price         as list_price,
+  ss_sales_price        as sales_price,
+  ss_net_paid           as net_paid,
+  ss_net_paid_inc_tax   as net_paid_inc_tax,
+  ss_net_profit         as net_profit
 from {{ source('tpcds', 'STORE_SALES') }}
